e605c8e7a5a2265935bc68bf8398f394e00622d2
David Chmura <dschmura@humbledaisy.com>
2018-09-02 13:10:32 -0400

    Initial Commit

---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+
+  server "jobjourney.com", user: 'deployer', roles: %w{app db web}
+  set :stage, :production
+  set :branch, :master
+  set :deploy_user, "deployer"
+  set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/jobjourney"
+  set :branch, 'master'
+  set :rails_env, 'production'
+  set :puma_env, "production"
+  set :puma_conf, "#{shared_path}/config/puma.rb"
---
+++ config/puma.sample.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env puma
+
+directory '/home/deployer/apps/jobjourney/current'
+rackup "/home/deployer/apps/jobjourney/current/config.ru"
+environment 'production'
+
+pidfile "/home/deployer/apps/jobjourney/shared/tmp/pids/puma.pid"
+state_path "/home/deployer/apps/jobjourney/shared/tmp/pids/puma.state"
+stdout_redirect '/home/deployer/apps/jobjourney/current/log/puma.error.log', '/home/deployer/apps/jobjourney/current/log/puma.access.log', true
+
+threads 4,16
+
+bind 'unix:///home/deployer/apps/jobjourney/shared/tmp/sockets/jobjourney-puma.sock'
+
+workers 0
+
+preload_app!
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "/home/deployer/apps/jobjourney/current/Gemfile"
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
